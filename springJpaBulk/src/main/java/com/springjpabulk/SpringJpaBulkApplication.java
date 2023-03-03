8e8bd145abf9f82b6f3738daa8d3fcbe2b7683d1
hyungjae.lee <dlgudwo11@gmail.com>
2023-03-03 14:51:53 +0900

    Spring JPA BULK 비교

---
+++ springJpaBulk/src/main/java/com/springjpabulk/SpringJpaBulkApplication.java
@@ -0,0 +1,13 @@
+package com.springjpabulk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBulkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaBulkApplication.class, args);
+    }
+
+}
---
+++ springJpaBulk/src/test/java/com/springjpabulk/SpringJpaBulkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpabulk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBulkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
