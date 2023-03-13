c2f6646761d9ac45839bdfcbb15fc3310941c002
hyungjae.lee <dlgudwo11@gmail.com>
2023-03-13 09:49:46 +0900

    Spring EventPublisher
     - 프로젝트추가

---
+++ springEventPublisher/src/main/java/com/springeventpublisher/SpringEventPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.springeventpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventPublisherApplication.class, args);
+    }
+
+}
---
+++ springEventPublisher/src/test/java/com/springeventpublisher/SpringEventPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springeventpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
