c89c7991cd9cb9520898d9b7fd8f448410c56a35
hyungjae.lee <dlgudwo11@gmail.com>
2023-04-21 14:57:41 +0900

    Spring Controller Advice
     - init commit

---
+++ springControllerAdivce/src/main/java/com/springcontrolleradivce/SpringControllerAdivceApplication.java
@@ -0,0 +1,13 @@
+package com.springcontrolleradivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringControllerAdivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringControllerAdivceApplication.class, args);
+    }
+
+}
---
+++ springControllerAdivce/src/test/java/com/springcontrolleradivce/SpringControllerAdivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcontrolleradivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringControllerAdivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
