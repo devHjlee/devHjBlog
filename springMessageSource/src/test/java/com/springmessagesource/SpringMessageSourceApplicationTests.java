07a43209ed94dee499944ea698e2462d82b799d4
hyungjae.lee <dlgudwo11@gmail.com>
2023-09-14 14:23:36 +0900

    feat : ADD_WebSocket Client
     - 웹소켓 클라이언트 추가 (Nv,Okhttp3)

---
+++ springMessageSource/src/test/java/com/springmessagesource/SpringMessageSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmessagesource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessageSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
